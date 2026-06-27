5b01df7472bcd7ed9f9bf15f63ccb219ed5e40da
schokett <skrollanelenazimmermann@gmail.com>
2026-06-28 01:36:26 +0200

    border changed

--- src/components/smallcard/SmallCard.tsx
+++ src/components/smallcard/SmallCard.tsx
@@ -2,19 +2,22 @@ import "./smallcard.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretUp, faUserGroup, type IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
-interface ContentProps{
-clasName?: string;
-icon?: IconDefinition;
-iconBg?: string;
-iconColor?: string;
-content: string;
-subtitle?: string;
+interface ContentProps {
+  clasName?: string;
+  icon?: IconDefinition;
+  iconBg?: string;
+  iconColor?: string;
+  content: string;
+  subtitle?: string;
 }
 
-function content({ clasName, icon, iconBg, iconColor, content, subtitle }: ContentProps){
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(e.target.value);
-  };
+// Ich wollte die smallcard function dynmaisch umschreiben.-
+// upnote://x-callback-url/openNote?noteId=019f0ab5-d509-721d-a32f-5e912edd0696
+
+// function SmallCard({ clasName, icon, iconBg, iconColor, content, subtitle }: ContentProps){
+//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     onChange(e.target.value);
+//   };
 
 function SmallCard() {
   return (
@@ -28,9 +31,11 @@ function SmallCard() {
           4,2%
         </div>
       </div>
+      <div className="smallcard__main-container">
+        <div className="smallcard__content">1.247</div>
+        <div className="smallcard__content">Nutzer gesamt</div>
+      </div>
     </div>
   );
 }
 export default SmallCard;
-
-// Ich wollte die smallcard function dynmaisch umschreiben!!!
